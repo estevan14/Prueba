2d6d06f2af61773fe41afc9dcbe9b910cbf02182
STEBAN <STEBAN@DESKTOP-EP71O4G>
2016-10-31 20:50:48 -0500

    prueba 2

--- src/taller/Taller.java
+++ src/taller/Taller.java
@@ -25,7 +25,7 @@ public class Taller {
         double correcto;
         Scanner leer = new Scanner(System.in);
 
-        System.out.print("Ingrese una  IP: ");
+        System.out.print("Ingrese una direcion  IP: ");
         ip = leer.next();
         validar = ip.split("\\.");
         System.out.printf("Elementos de la Ip: %s\n", Arrays.toString(validar));
